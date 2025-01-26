bcf1ee5dd0ae1b66fc8d87b1e3ce0e9c16f0f4b1
vishal lokhande <lvishal108@gmail.com>
2025-01-26 08:18:24 +0000

    chapter 1 practice

--- main.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-
-print("The sum of the numbers is:", a + b)
-print("The product of the numbers is:", a * b)
-print("The difference of the numbers is:", a - b)
-print("The quotient of the numbers is:", a / b)
-print("The remainder of the numbers is:", a % b)    
