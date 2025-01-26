f713b5e4e56570c28141662a9d4aa10617a28754
vishal lokhande <lvishal108@gmail.com>
2025-01-26 08:33:16 +0000

    chapter 2 practice

---
+++ chapter2.py
@@ -0,0 +1,31 @@
+# que 1
+# a = int(input("number 1 : "))
+# b = int(input("number 2 : "))
+# print("num 1 + num 2 = " ,a+b)
+
+# que 2
+# a = int(input("enter a number : "))
+# print(a/2)
+
+# # que 3
+# a = (input("enter : "))
+# print(type(a))
+
+# # que 4
+# a = 30
+# b= 40
+# if a>b:
+#     print("a is greater than b")
+# else:
+#     print("a is lesser than b")
+
+# # que 5
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+
+# c = (a+b)/2
+# print(c)
+
+# # que 6
+# a = int(input("enter a number : "))
+# print(a**2)
