0f8229cafba60ed792edbddea8a3f3cb38cc3372
vishal lokhande <lvishal108@gmail.com>
2025-01-28 05:12:12 +0000

    User input hour determines greeting.

--- chapter2.py
+++ chapter2.py
@@ -29,3 +29,26 @@
 # # que 6
 # a = int(input("enter a number : "))
 # print(a**2)
+
+
+# This code takes an input hour from the user, compares it with the current system time, and prints a greeting ("Good morning," "Good afternoon," "Good evening," or "Good night") based on the given hour.
+
+
+# hour = int(input("enter a time : "))
+# import time
+# timestamp = time.strftime('%H:%M:%S')
+# a = int(timestamp.split(':')[0])
+
+
+# if hour >= 5 and  hour  <=10 :
+#     print(("Good morning"))
+# elif hour >11 and hour <=16 :
+#     print("good afternoon")
+# elif hour > 17 and hour <= 20:
+#     print("good evening")
+# elif hour > 21 and hour <= 24:
+#     print ("good Night ")
+# elif hour > 0 and hour <5 :
+#     print("good Night")
+# else:
+#     print("enter a valid time ")
