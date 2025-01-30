b487f11b836e72923847af858e696c08a53d8f11
vishal lokhande <lvishal108@gmail.com>
2025-01-30 18:22:05 +0000

    positive indexing

---
+++ pos_neg_indxing.py
@@ -0,0 +1,8 @@
+# List for demonstration
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+# Positive Indexing (Indexing from the beginning, starting from 0)
+print("Positive Indexing:")
+print(f"Element at index 0 (first element): {fruits[0]}")  # First element (apple)
+print(f"Element at index 1 (second element): {fruits[1]}")  # Second element (banana)
+print(f"Element at index 4 (fifth element): {fruits[4]}")  # Fifth element (elderberry)
