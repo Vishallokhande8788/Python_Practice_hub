acbd0dac12b17b4cc0803065c248f416127fd7c2
vishal lokhande <lvishal108@gmail.com>
2025-03-03 15:14:32 +0000

    function practice

---
+++ QST Class/Function.py
@@ -0,0 +1,53 @@
+print("Hello World")
+# print("Hello World")  # this is a comment
+def greet():
+    print("Hello World")
+
+greet()
+
+# add function
+def addition(a, b):
+        print(a + b)
+addition(1, 2)
+
+# count even number
+def even_number():
+    user_input = int(input("how many even numbers do you want to find: "))
+    for i in range(1, user_input + 1):
+        if i % 2 == 0:
+            print(i)
+even_number()
+
+# age calculator
+def calc_age(birth_year):
+    current_year = 2025
+    age = current_year - birth_year
+    print(`tour age is {age} years`)
+
+calc_age(2003)
+
+# def f1(x):
+#     b=x**2
+#     print(b)
+
+#     def start():
+#         x=5
+#         f1(x)
+# start()
+
+# given errob because of b is a local scope variable
+
+# correct code
+def f1(y):
+    b=y**2
+    return b
+
+def start():
+        x=5
+        return_var=f1(x)
+        print(return_var)
+start()
+
+
+
+
