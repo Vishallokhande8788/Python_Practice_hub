3757502932cd7c2c4e42144fda51280a4f076ad3
vishal lokhande <lvishal108@gmail.com>
2025-03-10 08:13:46 +0000

    practice number system bin ,oct , hex

---
+++ Qst Class D sir/Number_System.py
@@ -0,0 +1,49 @@
+a = 1234
+print(type(a))
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+a = 0o1234567
+print(a)
+print(type(a))
+
+a=0x123face
+print(a)
+print(type(a))
+
+# base conversion function
+#  # bin(): to convert any base to binary base
+
+print(bin(123))
+print(bin(0o123))
+print(bin(0xface123))
+
+#  # oct(): to convert any base to octal base
+
+print(oct(123))
+print(oct(0b1010))
+print(oct(0xface123))
+#  # hex(): to convert any base to hexadecimal base
+
+print(hex(123))
+print(hex(0b1010))
+print(hex(0o123))
+
+a= 10.5
+print(a)
+print(type(a))
+
+# for fload data type only decimal number system is allowed
+# a=0b1010.0b1010
+# print(a)
+# print(type(a))
+
