d0eaadf01bb59f1e99aa5a6968b1565997e1c575
vishal lokhande <lvishal108@gmail.com>
2025-03-10 08:28:56 +0000

    decinal number + complex Number

--- Qst Class D sir/Number_System.py
+++ Qst Class D sir/Number_System.py
@@ -47,3 +47,88 @@ print(type(a))
 # print(a)
 # print(type(a))
 
+a = 10+20j
+print(a)
+print(type(a))
+
+a = 10+20j
+print(a)
+print(type(a))
+
+# a = 10+20d
+# print(a)
+# print(type(a))
+# its give an error because variable change j to d
+
+# for real part any number system is allowded
+
+a=0b101010+20j
+print(a)
+print(type(a))
+
+# for imaginary part only decimal system is allowded
+# a= 10+0b1011j
+# print(a)
+# print(type(a))
+
+a=10+20j
+b=30+40j
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+# print(a%b) type error :cannot mode complex number
+
+
+a=10+20j
+print(a.real)
+print(a.imag)
+
+a= True+10j
+print(a)
+
+# a= true + 10j
+# print(a)
+# error
+
+# bool
+a=True
+print(a)
+print(type(a))
+
+a=False
+print(a)
+print(type(a))
+
+# a = true NameError: name 'true' is not defined
+# print(a)
+# print(type(a))
+
+print(True+True)
+print(True+False)
+print(False+True)
+print(False+False)
+print(True-False)
+print(False-5)
+
+name = 'John'
+print(name)
+print(type(name))
+
+
+name= 'V'
+print(name)
+print(type(name))
+
+name = "Vishal"
+print(name)
+print(type(name))
+
+name=''' hello everyone 
+        good morning
+        we learn python
+        in today's class
+        
+        '''
+print(name)
+print(type(name))
