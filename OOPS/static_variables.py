4906ec8da2007041b771c82088f8fb350669a977
vishal lokhande <lvishal108@gmail.com>
2025-04-15 14:56:52 +0000

    practic eg

--- OOPS/static_variables.py
+++ OOPS/static_variables.py
@@ -42,6 +42,39 @@ print(Test.__dict__)
 
 
 
+#  eg create a bank account with bankname , account holder name , account nno , balance , below that add to options withdrawl option  balance is 500 and witdraww 1000 then pring msg your balance is insufficient , deposite option , print deposite msg your ac/no xyz has been created with 1000 s now your total balance is previous balance + 1000 
+
+
+class Bank:
+    def __init__(self, bank_name, acc_holder_name, acc_no, acc_balance):
+        self.bank_name = bank_name
+        self.acc_holder_name = acc_holder_name
+        self.acc_no = acc_no
+        self.acc_balance = acc_balance
+
+    def display_details(self):
+        print('Bank Name:', self.bank_name)
+        print('Account Holder Name:', self.acc_holder_name)
+        print('Account No:', self.acc_no)
+        print('Account Balance:', self.acc_balance)
+
+    def l1(self, withdrawl_amount):
+        if withdrawl_amount > self.acc_balance:
+            print("Insufficient Balance")
+        else:
+            self.acc_balance -= withdrawl_amount
+            print(f"{withdrawl_amount} withdrawn. New balance: {self.acc_balance}")
+
+    def l2(self, deposite_amount):
+        self.acc_balance += deposite_amount
+        print(f"{deposite_amount} deposited. New balance: {self.acc_balance}")
+
+
+# Test the class
+b = Bank('SBI', 'Vishal', '123456789', 500)
+b.l1(200)
+b.l2(100)
+b.display_details()
 
 
 
