088ab3f510d48702bcd0f011aa3fa8fbe424cc5c
vishal lokhande <lvishal108@gmail.com>
2025-05-06 14:53:17 +0000

    practicing tkinter basic calculator

---
+++ tkinter__practice/basiccalculator.py
@@ -0,0 +1,128 @@
+from tkinter import *
+root =Tk()
+root.geometry("700x300")
+root.title("this is my first tkinter frame")
+# b=Button(
+#     text="click" ,
+#     fg='red' ,
+#     bg='black',
+#     font='Arial 16 bold',
+#     borderwidth=6,
+#     relief='sunken'
+# )
+#
+# b2 = Button(
+# text="click" ,
+#     fg='red' ,
+#     bg='black',
+#     font='Arial 16 bold',
+#     borderwidth=6,
+#     relief='sunken'
+# )
+#
+# b3 = Button(
+# text="click" ,
+#     fg='red' ,
+#     bg='black',
+#     font='Arial 16 bold',
+#     borderwidth=6,
+#     relief='sunken'
+# )
+#
+# b4 = Button(
+#     text="click" ,
+#     fg='red' ,
+#     bg='black',
+#     font='Arial 16 bold',
+#     borderwidth=6,
+#     relief='sunken'
+# )
+
+# b.grid(row=0, column=0)
+# b2.grid(row=1, column=1)
+# b3.grid(row=2, column=2)
+# b4.grid(row=3, column=3)
+
+
+#  basic calculator
+msg = Label(
+    text='No 1',
+    font=('Times New Roman', 20, 'bold'),
+    padx=100,
+    pady=10,
+)
+input = Entry(
+    borderwidth=6,
+    relief = 'sunken'
+
+)
+
+msg1 = Label(
+    text='No 2',
+    font=('Times New Roman', 20, 'bold'),
+    padx=100,
+    pady=10,
+
+)
+input1 = Entry(
+    borderwidth=6,
+    relief = 'sunken'
+
+)
+msg2 = Label(
+    text='RESULT',
+    font=('Times New Roman', 20, 'bold'),
+    padx=100,
+    pady=10,
+
+)
+input2 = Entry(
+    borderwidth=6,
+    relief = 'sunken'
+
+)
+
+b = Button(
+    text='+',
+    padx=30,
+    pady=10
+)
+
+b1 = Button(
+    text='-',
+    padx=30,
+    pady=10
+)
+
+b2 = Button(
+    text='*',
+    padx=30,
+    pady=10
+)
+b3 = Button(
+    text='/',
+    padx=30,
+    pady=10
+)
+
+
+msg.grid(row=0, column=10)
+input.grid(row=0 , column=12)
+
+msg1.grid(row=1, column=10)
+input1.grid(row=1 , column=12)
+
+msg2.grid(row=2, column=10)
+input2.grid(row=2 , column=12)
+
+b.grid(row = 3 , column=9)
+b1.grid(row = 3 , column=10)
+b2.grid(row = 3 , column=12)
+b3.grid(row =  3, column=14)
+
+
+
+root.mainloop()
+
+
+
