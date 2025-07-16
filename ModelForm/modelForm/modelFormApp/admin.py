05a8ad61fb1208b6dafa2eb8a8f2fa7e77077996
vishal lokhande <lvishal108@gmail.com>
2025-07-16 05:48:10 +0000

    solve promblem of image rendering ,

--- ModelForm/modelForm/modelFormApp/admin.py
+++ ModelForm/modelForm/modelFormApp/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from modelFormApp.models import modelForm
 
-# Register your models here.
-
 class modelFormAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age', 'gender', 'address', 'image')
+    list_display = ('name', 'age', 'gender', 'address', 'show_image')
+
+    def show_image(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="20" height="20" style="object-fit: cover;" />', obj.image.url)
+        return "No Image"
+
+    show_image.short_description = 'Profile Image'
 
 admin.site.register(modelForm, modelFormAdmin)
--- ModelForm/modelForm/modelFormApp/models.py
+++ ModelForm/modelForm/modelFormApp/models.py
@@ -7,3 +7,4 @@ class modelForm(models.Model):
     gender = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
+
