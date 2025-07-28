ad7fd51622d57ced3b16afc24fb0b6d3eda5d4d3
vishal lokhande <lvishal108@gmail.com>
2025-07-28 15:51:55 +0000

    imp settings for user authentication , render htmls files

--- userauthenticationLoginLogout/userauth/userauthapp/views.py
+++ userauthenticationLoginLogout/userauth/userauthapp/views.py
@@ -5,6 +5,13 @@ def home(request):
     return render(request, 'userauthapp/home.html')
 
 
-@ login_required
+@login_required
 def pyExam(request):
     return render(request, 'userauthapp/python.html')
+
+@login_required
+def javaExam(request):
+    return render(request, 'userauthapp/java.html')
+
+def jsExam(request):
+    return render(request, 'userauthapp/js.html')   
