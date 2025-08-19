92c138bfac417bfdfc361783d4de5d5cd6e1bb52
vishal lokhande <lvishal108@gmail.com>
2025-08-19 04:10:15 +0000

    add ChaiVarity model , register in admin.py ,

--- DjangoWithChaiOrCode/myproject/chaiorcode/chaiorcode/urls.py
+++ DjangoWithChaiOrCode/myproject/chaiorcode/chaiorcode/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path , include 
+from django.conf import settings
+from django.conf.urls.static import static
 from chaiapp import views  
 
 urlpatterns = [
@@ -30,4 +32,4 @@ urlpatterns = [
 
     path("__reload__/", include("django_browser_reload.urls")),
 
-]
+]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
