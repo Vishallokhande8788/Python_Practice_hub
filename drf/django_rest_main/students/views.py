73460105be2792628ef0efeabc64af83a1172e79
vishal lokhande <lvishal108@gmail.com>
2025-10-07 06:39:00 +0000

    drf setup, normal view run done

---
+++ drf/django_rest_main/students/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def students(request):
+    students_list = [
+        {
+            'id': 1,
+            'name': 'John Doe',
+            'age': 20,
+            'grade': 'A'
+            
+        }
+    ]
+    return HttpResponse(students_list)
