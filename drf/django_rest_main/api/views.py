d504b13f8fcc703f809be686d4e5fedf4827793d
vishal lokhande <lvishal108@gmail.com>
2025-10-10 06:20:52 +0000

    add manual serilization

--- drf/django_rest_main/api/views.py
+++ drf/django_rest_main/api/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 # Create your views here.
 def studentsView(request):
-    students_list = {
-        'id':1,
-        'name':'John',
-        'age':20,
-        'gender':'Male'
+    students = Student.objects.all()
+    students_list = list(students.values())
 
-    }
-
-    return JsonResponse(students_list)
+    return JsonResponse(students_list, safe=False)
 
