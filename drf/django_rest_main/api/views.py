6a2b2e5aaa043d56fba54433c658b9cb135f56f9
vishal lokhande <lvishal108@gmail.com>
2025-10-13 07:33:18 +0000

    update and delete employy by using put and delete request

--- drf/django_rest_main/api/views.py
+++ drf/django_rest_main/api/views.py
@@ -89,11 +89,25 @@ class Employees(APIView):
 class EmployeesDetail(APIView):
     def get_object (self, pk):
         try:
-            return Employee.objects.get(pk=pk)
+            return Employee.objects.get(id=pk)
         except Employee.DoesNotExist:
             raise Http404 
 
     def get(self,request, pk):
         employee = self.get_object(pk) 
         serializer = EmployeeSerializer(employee)
-        return Response(serializer.data, status=status.HTTP_200_OK)                                                                 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
