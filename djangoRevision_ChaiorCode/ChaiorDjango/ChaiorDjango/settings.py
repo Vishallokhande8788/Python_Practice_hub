804ac82d3533bec248cf8719427660931c897434
vishal lokhande <lvishal108@gmail.com>
2026-03-07 07:09:50 +0000

    integrate tailwind css setup

--- djangoRevision_ChaiorCode/ChaiorDjango/ChaiorDjango/settings.py
+++ djangoRevision_ChaiorCode/ChaiorDjango/ChaiorDjango/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "Chai",
+    "tailwind",
+    "apptailwind",
+
 ]
+TAILWIND_APP_NAME = "apptailwind"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
---
+++ djangoRevision_ChaiorCode/ChaiorDjango/apptailwind/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApptailwindConfig(AppConfig):
+    name = 'apptailwind'
